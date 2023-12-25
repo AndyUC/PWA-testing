725d5130cd9d86bb2e0423542612a88fa15989cd
DucLVN <nhanduc811@gmail.com>
2023-12-25 11:46:22 +0700

    update-2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     const client = axios.create({
       baseURL: "https://pwa-tesing.onrender.com"
     });
-    let subscription: any
+    let subscription = {}
     if ((!localStorage.getItem("subscription")) || (localStorage.getItem("subscription") === "{}")) {
       console.log('notification in window', 'Notification' in window, "serviceWorker in navigator", 'serviceWorker' in navigator)
       if ('Notification' in window && 'serviceWorker' in navigator) {
@@ -18,19 +18,21 @@ function App() {
 
             navigator.serviceWorker.ready.then(async function (registration) {
               // Đăng ký sự kiện push với service worker
-              try {
-                subscription = await registration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: "BPnBnQ3-OJ3fwA51Ispz7srlLRefQdKQipkjA4fnzYJnIim4Wc0LHF3-Z-KafgK_XxrO-8eI6rvpbvQ7h12qreo" })
-                if (!subscription) {
-                  window.alert("no subscription")
-                } else {
-                  console.log(subscription)
-                  const dataToSend = { subscription: subscription }
-                  localStorage.setItem("subscription", JSON.stringify(dataToSend))
+                   await registration.pushManager.subscribe({ userVisibleOnly: true, applicationServerKey: "BPnBnQ3-OJ3fwA51Ispz7srlLRefQdKQipkjA4fnzYJnIim4Wc0LHF3-Z-KafgK_XxrO-8eI6rvpbvQ7h12qreo" })
+                   .then(async subscription=>{
+                  if (!subscription) {
+                    window.alert("no subscription")
+                  } else {
+                    console.log(subscription)
+                    const dataToSend = { subscription: subscription }
+                    localStorage.setItem("subscription", JSON.stringify(dataToSend))
+                    await client.post('/api/v1/push', {
+                      subscription: subscription
+                  }).catch(e=>console.log(e))
                 }
-              } catch (error) {
-                console.error('Lỗi khi đăng ký push:', error);
-              }
-            }).catch((error) => console.log(error));
+              }).catch(e=>console.error('Lỗi khi đăng ký push:', e)) 
+                }
+            ).catch((error) => console.log(error));
           }
         });
       }
@@ -38,20 +40,12 @@ function App() {
       console.log(localStorage.getItem("subscription"))
       subscription = JSON.parse(localStorage.getItem("subscription") || "{}")
       console.log("subcription", subscription)
+      await client.post('/api/v1/push', {
+        subscription: subscription
+    })
 
     }
-
-    if (Object.getOwnPropertyNames(subscription).length <= 0) {
-      window.alert('no object here')
-    } else {
-      try {
-        await client.post('/api/v1/push', {
-          subscription: subscription
-      })
-      } catch (error) {
-        console.log(error)
-      }
-    }
+    
   }
 
   return (
